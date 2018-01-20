0e673a7f9ddc40fa6655195d6df86c847fb2e5ef
xish <tyler.boulware@gmail.com>
2018-01-20 00:29:49 -0600

    temporarily remove old-style actions to avoid conflicts with new action system until they can be reimplemented with the new action system.

--- main.py
+++ main.py
@@ -41,113 +41,113 @@ while text_input != 'q':
 
 	# Syntax:
 	#	kill TARGET
-	if command[0] == 'kill':
-		if parameters:
-			target_name = parameters
-		if not parameters:
-			print("Who would you like to kill?")
-			target_name = input('> (kill) ')
-
-		target_list = m_group.find_member_by_name(people, target_name)
-		if m_group.check_target(target_list, people, target_name) == m_group.e_target_type.unique:
-			event = player.kill(*target_list, world_time)
-			events.append(event)
+#	if command[0] == 'kill':
+#		if parameters:
+#			target_name = parameters
+#		if not parameters:
+#			print("Who would you like to kill?")
+#			target_name = input('> (kill) ')
+#
+#		target_list = m_group.find_member_by_name(people, target_name)
+#		if m_group.check_target(target_list, people, target_name) == m_group.e_target_type.unique:
+#			event = player.kill(*target_list, world_time)
+#			events.append(event)
 
 	# Syntax:
 	#	tell TARGET "UTTERANCE"
-	if command[0] == 'tell':
-		bad_syntax = False
-
-		if parameters:
-			if parameters.count('\"') == 2:
-				# We've got one and only one valid quote-enclosed utterance.
-				# Now let's take a slice of everything before that and use that as the target name
-				opening_quote = parameters.find('\"')
-				closing_quote = parameters.rfind('\"')
-				target_name = parameters[:opening_quote].rstrip()
-				# (SELF) Why is it +1 for opening_quote but nothing for closing_quote? It works, but I don't know why
-				utterance = parameters[opening_quote+1 : closing_quote]
-			else:
-				bad_syntax = True
-				print("Incorrect syntax. Try: tell TARGET \"UTTERANCE\"")
-		else:
-			print("Who would you like to tell?")
-			target_name = input('> (tell) ')
-			print("What would you like to say?")
-			utterance = input('> (tell {}) '.format(target_name))
-
-		if not bad_syntax:
-			target_list = m_group.find_member_by_name(people, target_name)
-			if m_group.check_target(target_list, people, target_name) == m_group.e_target_type.unique:
-				event = player.tell(*target_list, utterance, world_time)
-				events.append(event)
-
-	# Syntax:
-	#	give ITEM to TARGET
-	if command[0] == 'give':
-		bad_syntax = False
-
-		if parameters:
-			to_delimiter = ' to '
-			to_location = parameters.rfind(' to ')
-			if to_location != -1:
-				item_name = parameters[:to_location].strip().lower()
-				target_name = parameters[to_location+len(to_delimiter):].strip().lower()
-			else:
-				bad_syntax = True
-				print("Incorrect syntax. Try: give ITEM to TARGET")
-		else:
-			print("What would you like to give?")
-			item_name = input('> (give) ')
-			print("Who would you like to give to?")
-			target_name = input('> (give {} to) '.format(item_name))
-
-		if not bad_syntax:
-			item_list = m_group.find_member_by_name(items, item_name)
-			target_list = m_group.find_member_by_name(people, target_name)
-			if m_group.check_target(item_list, items, item_name) == m_group.check_target(target_list, people, target_name) == m_group.e_target_type.unique:
-				event = player.give(*item_list, *target_list, world_time)
-				events.append(event)
-
-	# Syntax:
-	#	take ITEM from TARGET
-	if command[0] == 'take':
-		bad_syntax = False
-
-		if parameters:
-			from_delimiter = ' from '
-			from_location = parameters.rfind(' from ')
-			if from_location != -1:
-				item_name = parameters[:from_location].strip().lower()
-				target_name = parameters[from_location+len(from_delimiter):].strip().lower()
-			else:
-				bad_syntax = True
-				print("Incorrect syntax. Try: take ITEM from TARGET")
-		else:
-			print("What would you like to take?")
-			item_name = input('> (take) ')
-			print("Whom would you like to take it from?")
-			target_name = input('> (take {} from) '.format(item_name))
-
-		if not bad_syntax:
-			item_list = m_group.find_member_by_name(items, item_name)
-			target_list = m_group.find_member_by_name(people, target_name)
-			if m_group.check_target(item_list, items, item_name) == m_group.check_target(target_list, people, target_name) == m_group.e_target_type.unique:
-				event = player.take(*item_list, *target_list, world_time)
-				events.append(event)
-
-	# Syntax:
-	#	laugh
-	if command[0] == 'laugh':
-		bad_syntax = False
-
-		if parameters:
-			bad_syntax = True
-			print("Incorrect syntax. Try: laugh")
-
-		if not bad_syntax:
-			event = player.laugh(world_time)
-			events.append(event)
+#	if command[0] == 'tell':
+#		bad_syntax = False
+#
+#		if parameters:
+#			if parameters.count('\"') == 2:
+#				# We've got one and only one valid quote-enclosed utterance.
+#				# Now let's take a slice of everything before that and use that as the target name
+#				opening_quote = parameters.find('\"')
+#				closing_quote = parameters.rfind('\"')
+#				target_name = parameters[:opening_quote].rstrip()
+#				# (SELF) Why is it +1 for opening_quote but nothing for closing_quote? It works, but I don't know why
+#				utterance = parameters[opening_quote+1 : closing_quote]
+#			else:
+#				bad_syntax = True
+#				print("Incorrect syntax. Try: tell TARGET \"UTTERANCE\"")
+#		else:
+#			print("Who would you like to tell?")
+#			target_name = input('> (tell) ')
+#			print("What would you like to say?")
+#			utterance = input('> (tell {}) '.format(target_name))
+#
+#		if not bad_syntax:
+#			target_list = m_group.find_member_by_name(people, target_name)
+#			if m_group.check_target(target_list, people, target_name) == m_group.e_target_type.unique:
+#				event = player.tell(*target_list, utterance, world_time)
+#				events.append(event)
+#
+#	# Syntax:
+#	#	give ITEM to TARGET
+#	if command[0] == 'give':
+#		bad_syntax = False
+#
+#		if parameters:
+#			to_delimiter = ' to '
+#			to_location = parameters.rfind(' to ')
+#			if to_location != -1:
+#				item_name = parameters[:to_location].strip().lower()
+#				target_name = parameters[to_location+len(to_delimiter):].strip().lower()
+#			else:
+#				bad_syntax = True
+#				print("Incorrect syntax. Try: give ITEM to TARGET")
+#		else:
+#			print("What would you like to give?")
+#			item_name = input('> (give) ')
+#			print("Who would you like to give to?")
+#			target_name = input('> (give {} to) '.format(item_name))
+#
+#		if not bad_syntax:
+#			item_list = m_group.find_member_by_name(items, item_name)
+#			target_list = m_group.find_member_by_name(people, target_name)
+#			if m_group.check_target(item_list, items, item_name) == m_group.check_target(target_list, people, target_name) == m_group.e_target_type.unique:
+#				event = player.give(*item_list, *target_list, world_time)
+#				events.append(event)
+#
+#	# Syntax:
+#	#	take ITEM from TARGET
+#	if command[0] == 'take':
+#		bad_syntax = False
+#
+#		if parameters:
+#			from_delimiter = ' from '
+#			from_location = parameters.rfind(' from ')
+#			if from_location != -1:
+#				item_name = parameters[:from_location].strip().lower()
+#				target_name = parameters[from_location+len(from_delimiter):].strip().lower()
+#			else:
+#				bad_syntax = True
+#				print("Incorrect syntax. Try: take ITEM from TARGET")
+#		else:
+#			print("What would you like to take?")
+#			item_name = input('> (take) ')
+#			print("Whom would you like to take it from?")
+#			target_name = input('> (take {} from) '.format(item_name))
+#
+#		if not bad_syntax:
+#			item_list = m_group.find_member_by_name(items, item_name)
+#			target_list = m_group.find_member_by_name(people, target_name)
+#			if m_group.check_target(item_list, items, item_name) == m_group.check_target(target_list, people, target_name) == m_group.e_target_type.unique:
+#				event = player.take(*item_list, *target_list, world_time)
+#				events.append(event)
+#
+#	# Syntax:
+#	#	laugh
+#	if command[0] == 'laugh':
+#		bad_syntax = False
+#
+#		if parameters:
+#			bad_syntax = True
+#			print("Incorrect syntax. Try: laugh")
+#
+#		if not bad_syntax:
+#			event = player.laugh(world_time)
+#			events.append(event)
 
 	for i, event in enumerate(events):
 		print("event {}: {}".format(i, event.action))
--- person.py
+++ person.py
@@ -11,35 +11,35 @@ class Person:
 		self.name = name.lower()
 		self.status = e_status.alive
 #		self.memories = [] # Maybe once we get more advanced
-	def kill(self, target, time):
-		if target.status == e_status.alive:
-			event = m_event.Event(time, self, m_event.e_action.kill, target)
-
-			target.status = e_status.dead
-			print("{} killed {} at {} o'clock.".format(self.name, target.name, time))
-		else:
-			event = m_event.null_event
-
-			print("{} is already dead.".format(target.name))
-
-		return event
-
-	def tell(self, target, utterance, time):
-		event = m_event.Event(time, self, m_event.e_action.tell, target)
-		print("{} told {} \"{}\" at {} o'clock.".format(self.name, target.name, utterance, time))
-		return event
-
-	def give(self, item, target, time):
-		event = m_event.Event(time, self, m_event.e_action.give, item, target)
-		print("{} gave {} to {} at {} o'clock.".format(self.name, item.name, target.name, time))
-		return event
-
-	def take(self, item, target, time):
-		event = m_event.Event(time, self, m_event.e_action.take, item, target)
-		print("{} took {} from {} at {} o'clock.".format(self.name, item.name, target.name, time))
-		return event
-
-	def laugh(self, time):
-		event = m_event.Event(time, self, m_event.e_action.laugh)
-		print("{} laughed at {} o'clock.".format(self.name, time))
-		return event
+#	def kill(self, target, time):
+#		if target.status == e_status.alive:
+#			event = m_event.Event(time, self, m_event.e_action.kill, target)
+#
+#			target.status = e_status.dead
+#			print("{} killed {} at {} o'clock.".format(self.name, target.name, time))
+#		else:
+#			event = None
+#
+#			print("{} is already dead.".format(target.name))
+#
+#		return event
+#
+#	def tell(self, target, utterance, time):
+#		event = m_event.Event(time, self, m_event.e_action.tell, target)
+#		print("{} told {} \"{}\" at {} o'clock.".format(self.name, target.name, utterance, time))
+#		return event
+#
+#	def give(self, item, target, time):
+#		event = m_event.Event(time, self, m_event.e_action.give, item, target)
+#		print("{} gave {} to {} at {} o'clock.".format(self.name, item.name, target.name, time))
+#		return event
+#
+#	def take(self, item, target, time):
+#		event = m_event.Event(time, self, m_event.e_action.take, item, target)
+#		print("{} took {} from {} at {} o'clock.".format(self.name, item.name, target.name, time))
+#		return event
+#
+#	def laugh(self, time):
+#		event = m_event.Event(time, self, m_event.e_action.laugh)
+#		print("{} laughed at {} o'clock.".format(self.name, time))
+#		return event
